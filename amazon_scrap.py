be9594fc84743aa574eb21f75e88159e916c88d6
durai <durai2599@gmail.com>
2022-10-13 19:24:51 +0530

    search button

--- amazon_scrap.py
+++ amazon_scrap.py
@@ -20,11 +20,18 @@ webdriver_service = Service(f"{homedir}/web_scrapping/chromedriver/stable/chrome
 
 
 driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
-url = ('https://www.amazon.in/gp/product/B09ZLPH6C8/ref=s9_acss_bw_cg_BAU21_8b1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-16&pf_rd_r=CA0CGGHQV7PXE160AV7B&pf_rd_t=101&pf_rd_p=e1070a0a-470e-4176-9d12-d9368b1f6428&pf_rd_i=1389396031&th=1')
+url = ('https://www.amazon.in')
 
 drive = driver.get(url)
-next_page = driver.find_element(By.LINK_TEXT, "See all reviews")
-next_page.click()
+search_tv = driver.find_element(By.NAME, "field-keywords")
+search_tv.send_keys("Sony Bravia 189 cm (75 inches) 4K Ultra HD Smart LED Google TV KD-75X80K (Black)")
+search_tv.send_keys(Keys.RETURN)
+
+tv = driver.find_element(By.LINK_TEXT, "Sony Bravia 189 cm (75 inches) 4K Ultra HD Smart LED Google TV KD-75X80K (Black)").click()
+driver.switch_to.window(driver.window_handles[1])
+
+next_page = driver.find_element(By.LINK_TEXT, "See all reviews").click()
+
 
 html = driver.page_source
 soup = BeautifulSoup(html, "lxml")
